c084843c1ae98e98f8643c88757e5e6a52bc9b16
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-09-19 21:32:07 +0300

    @NonNull annotation fixed

--- app/src/main/java/com/example/bookstory/DAO/Author.java
+++ app/src/main/java/com/example/bookstory/DAO/Author.java
@@ -1,10 +1,12 @@
 package com.example.bookstory.DAO;
 
+import androidx.annotation.NonNull;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
 @Entity
 public class Author {
+    @NonNull
     @PrimaryKey(autoGenerate = false)
     public String authorName;
 }
