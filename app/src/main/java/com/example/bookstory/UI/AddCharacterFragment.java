34e749b82dbd57dbb8fe8b6a76351b153cab1719
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-09-19 20:04:12 +0300

    move all fragments java classes to UI folder

--- app/src/main/java/com/example/bookstory/UI/AddCharacterFragment.java
+++ app/src/main/java/com/example/bookstory/UI/AddCharacterFragment.java
@@ -1,4 +1,4 @@
-package com.example.bookstory;
+package com.example.bookstory.UI;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.bookstory.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link BookListFragment#newInstance} factory method to
+ * Use the {@link AddCharacterFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class BookListFragment extends Fragment {
+public class AddCharacterFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class BookListFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public BookListFragment() {
+    public AddCharacterFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class BookListFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BookListFragment.
+     * @return A new instance of fragment AddCharacterFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static BookListFragment newInstance(String param1, String param2) {
-        BookListFragment fragment = new BookListFragment();
+    public static AddCharacterFragment newInstance(String param1, String param2) {
+        AddCharacterFragment fragment = new AddCharacterFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class BookListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book_list, container, false);
+        return inflater.inflate(R.layout.fragment_add_character, container, false);
     }
 }
