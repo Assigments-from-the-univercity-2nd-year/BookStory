f4930f8c0a37c725eb2e848f6eec264d333026e9
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-09-20 08:11:48 +0300

    RecyclerView works now

--- app/src/main/java/com/example/bookstory/DAO/Book.java
+++ app/src/main/java/com/example/bookstory/DAO/Book.java
@@ -12,4 +12,11 @@ public class Book {
     public int numberOfPages;
     public int yearOfPublication;
     public String annotation;
+
+    public Book(String bookName, int numberOfPages, int yearOfPublication, String annotation) {
+        this.bookName = bookName;
+        this.numberOfPages = numberOfPages;
+        this.yearOfPublication = yearOfPublication;
+        this.annotation = annotation;
+    }
 }
