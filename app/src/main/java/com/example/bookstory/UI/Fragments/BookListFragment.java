3d256d14d6dff3bb30cc0905fd1a21239d106059
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-09-21 20:13:09 +0300

    changed the cardView appearance in book_row and created FloatingActionButton in fragment_book_list.xml with action towards addBookFragment

--- app/src/main/java/com/example/bookstory/UI/Fragments/BookListFragment.java
+++ app/src/main/java/com/example/bookstory/UI/Fragments/BookListFragment.java
@@ -6,12 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bookstory.DOMAIN.DBController;
 import com.example.bookstory.R;
 import com.example.bookstory.UI.RecyclerViewAdapters.MyAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class BookListFragment extends Fragment {
 
@@ -20,6 +23,13 @@ public class BookListFragment extends Fragment {
                              Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_book_list, container, false);
         RecyclerView recyclerView = root.findViewById(R.id.recyclerView);
+        FloatingActionButton floatingActionButton = root.findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(v -> {
+            NavController navController = Navigation.findNavController(v);
+            BookListFragmentDirections.ActionBookListFragmentToAddBookFragment action =
+                    BookListFragmentDirections.actionBookListFragmentToAddBookFragment();
+            navController.navigate(action);
+        });
 
         DBController dbController = new DBController(getContext());
         //MOCK TEST
