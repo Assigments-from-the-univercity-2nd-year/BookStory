8307df78c5a4a3738813f3000db50396d1db7ef2
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-09-24 15:47:38 +0300

    rename some files

---
+++ app/src/main/java/com/example/bookstory/UI/Fragments/AddOrChangeBookFragment.java
@@ -0,0 +1,32 @@
+package com.example.bookstory.UI.Fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.bookstory.R;
+
+public class AddOrChangeBookFragment extends Fragment {
+
+    private View root;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        root = inflater.inflate(R.layout.fragment_add_or_change_book, container, false);
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+}
