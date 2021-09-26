a023133e2c9248cd629c37ece0350ffa0d7ff2e5
Mark Yavorskyi <yavorskiymark@gmail.com>
2021-09-26 19:43:20 +0300

    Did: implemented ArrayList sorted algo by Sortable.java interface

---
+++ app/src/main/java/com/example/bookstory/DOMAIN/Sortables/SortableArrayList.java
@@ -0,0 +1,14 @@
+package com.example.bookstory.DOMAIN.Sortables;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class SortableArrayList implements Sortable {
+
+    @Override
+    public <T> void sort(List<T> list, Comparator<? super T> c) {
+        ArrayList<T> arrayList = new ArrayList<>(list);
+        arrayList.sort(c);
+    }
+}
--- app/src/main/java/com/example/bookstory/DOMAIN/SortingController.java
+++ app/src/main/java/com/example/bookstory/DOMAIN/SortingController.java
@@ -1,19 +1,36 @@
 package com.example.bookstory.DOMAIN;
 
+import com.example.bookstory.DOMAIN.Sortables.Sortable;
 import com.example.bookstory.DOMAIN.enums.Criterion;
 import com.example.bookstory.DOMAIN.enums.Order;
 import com.example.bookstory.DOMAIN.enums.SortingAlgorithm;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class SortingController {
     public static <T> void sort(
             List<T> list,
             Comparator<? super T> comparator,
-            SortingAlgorithm sortingAlgorithm,
-            Order order,
-            Criterion... criteria) {
-        //TODO the realization
+            Class<? extends Sortable> sortingAlgoClass) {
+        try {
+            Constructor<? extends Sortable> constructor = sortingAlgoClass.getConstructor(sortingAlgoClass);
+            Sortable sortable = constructor.newInstance();
+            sortable.sort(list, comparator);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            Logger logger = Logger.getLogger("SortingController");
+            logger.log(Level.SEVERE, "The " + sortingAlgoClass.getCanonicalName() +
+                    " should have a public default constructor with no parameters.");
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
+            e.printStackTrace();
+            Logger logger = Logger.getLogger("SortingController");
+            logger.log(Level.SEVERE, "The " + sortingAlgoClass.getCanonicalName() +
+                    " should have a public de3fault constructor with no parameters.");
+        }
     }
 }
